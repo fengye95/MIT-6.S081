a3e76303719c340f39c59a73f27d38ff82ea69d3
fengye95 <2057190706@qq.com>
2025-12-11 18:47:00 +0800

    完成 Print a page table
    查看修改内容: git show HEAD~2

--- kernel/vm.c
+++ kernel/vm.c
@@ -432,3 +432,28 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void vmprint_helper(pagetable_t pagetable, int level) {
+  if (level >= 3) 
+    return;
+  
+  static char *indents[3] = { "..", ".. ..", ".. .. .." };
+
+  for(int i = 0; i < 512; i++) {
+    pte_t pte = pagetable[i];
+    if(pte & PTE_V) {
+      printf("%s%d: pte %p pa %p\n", indents[level], i, pte, PTE2PA(pte));
+      if((pte & (PTE_R | PTE_W | PTE_X)) == 0) {
+        vmprint_helper((pagetable_t)PTE2PA(pte), level + 1);
+      }
+    }
+  }
+}
+
+void
+vmprint(pagetable_t pagetable) {
+  
+  printf("page table %p\n", pagetable);
+
+  vmprint_helper(pagetable, 0);
+}
